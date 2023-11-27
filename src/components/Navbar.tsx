417695c6bda0c4c973b2113f98bccb0f02640069
Sebastián Águila <allarines@gmail.com>
2023-11-27 02:13:47 -0300

    borrado espacio

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ export default function Navbar() {
         <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.8rem] sm:h-[initial] sm:py-0">
           <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
             <motion.div
-              className=" h-full flex items-center ml-2"
+              className="h-full flex items-center ml-2"
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
